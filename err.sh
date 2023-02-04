23139c4f869e50acf1ad26fbf0b593bf8a44c651
lywdev <lywdev@lywdevs-MacBook-Pro.local>
2023-02-05 02:24:53 +0900

    testing

--- err.sh
+++ err.sh
@@ -3,11 +3,14 @@
 #  1>/dev/null : 표준 출력 내용은 버려라 (출력하지 말아라)
 # -c 1 : 1번만 체크해봐라 라는 의미
 
+echo "Type to ping IP "      // 0 일 경우 응답이 온 것이라 성공!
+read IP
+echo IP :$IP
+
 ping -c 1 192.168.0.1 1> /dev/null
 
 if [ $? == 0 ] 
 then 
-     echo "Gateway ping success!"      // 0 일 경우 응답이 온 것이라 성공!
 else
      echo "Gateway ping failed!"        // 응답이 없을 때 나타남
 fi
